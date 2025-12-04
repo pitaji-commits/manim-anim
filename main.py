74ad2b890f2c50ffaf9b937f279d0891c1165c46
pitaji-commits <shivamjuyal2023@gmail.com>
2025-12-04 18:07:39 +0530

    experimenting by creating tree structure in main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from manim import *
 from helper import MyVector
-
+from helper import MyNode
 
 
 def move_code(scene, code_string="", language="python"):
@@ -10,10 +10,13 @@ def move_code(scene, code_string="", language="python"):
 
     scene.play(code.animate.scale(0.6).to_edge(UR))
     scene.wait(1)
-    
-    
+
+
 class Test(Scene):
     def construct(self):
-        sq = Square()
-        self.play(Write(sq))
+        data = [1, 2, 3, 4, 5, 6, 7, 8]
+        nodes = VGroup(*[MyNode(value=val, is_rect=False) for val in data]).arrange(
+            RIGHT
+        )
+        self.play(Write(nodes))
         self.wait(1)
