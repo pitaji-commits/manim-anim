5c004b8d3737186f956fdcbf0364e1aa79b90baa
pitaji-commits <shivamjuyal2023@gmail.com>
2025-12-04 16:59:38 +0530

    creating Merge_2_way video in main.py

--- main.py
+++ main.py
@@ -2,3 +2,44 @@ from helper import MyVector
 
 
 
+MERGE_2_CODE = """
+def merge(data, low, mid, high):
+    left = data[:mid]
+    right = data[mid:]
+    i, j, k = 0, 0, 0
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            data[k] = left[i]
+            i += 1
+        else:
+            data[k] = right[j]
+            j += 1
+        k += 1
+
+    while i < len(left):
+        data[k] = left[i]
+        i += 1
+        k += 1
+
+    while j < len(right):
+        data[k] = right[j]
+        j += 1
+        k += 1
+"""
+
+
+def move_code(scene, code_string="", language="python"):
+    code = Code(code_string=code_string, language=language).scale(0.8)
+    scene.play(Write(code))
+    scene.wait(1)
+
+    scene.play(code.animate.scale(0.6).to_edge(UR))
+    scene.wait(1)
+    
+    
+class Test(Scene):
+    def construct(self):
+        move_code(self, MERGE_2_CODE)
+
+        
